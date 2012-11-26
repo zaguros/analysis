ecb63ab65a9cf508d7b225e2c5f1ec4543381a43
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2012-11-26 18:36:58 +0100

    added possibility to get the likelihood distributions for the probabilities of the multinomials distributions; also, XX and ZZ can done via independent multinomial distributions now

--- scripts/lde/entanglementfidelity_v2/MLE.py
+++ scripts/lde/entanglementfidelity_v2/MLE.py
@@ -25,22 +25,22 @@ class Emily:
                 time.strftime('%Y%m%d')+'-ldefidelity') 
         
         # psi2 w=200
-        #self.N_ZZ = np.array([13,95,79,21], dtype=int)
-        #self.N_XX = np.array([13,46,46,31], dtype=int)
-        #self.N_XmX = np.array([36,20,20,45], dtype=int)
+        self.N_ZZ = np.array([13,95,79,21], dtype=int)
+        self.N_XX = np.array([13,46,46,31], dtype=int)
+        self.N_XmX = np.array([36,20,20,45], dtype=int)
 
         # psi1 w = 80
-        self.N_ZZ = np.array([5,72,51,34], dtype=int)
-        self.N_XX = np.array([39,30,18,54], dtype=int)
-        self.N_XmX = np.array([13,41,38,29], dtype=int)
+        #self.N_ZZ = np.array([5,72,51,34], dtype=int)
+        #self.N_XX = np.array([39,30,18,54], dtype=int)
+        #self.N_XmX = np.array([13,41,38,29], dtype=int)
 
-        self.state = 'psi1'
+        self.state = 'psi2'
 
         # readout fidelities
-        self.F0_1 = 0.923
-        self.F1_1 = 0.996
-        self.F0_2 = 0.818
-        self.F1_2 = 0.996
+        self.F0_1 = 0.921
+        self.F1_1 = 0.997
+        self.F0_2 = 0.822
+        self.F1_2 = 0.989
 
         self.prob_pts = 101
         self.ab_pts = 101
@@ -48,6 +48,8 @@ class Emily:
         self.result_bins = 50
         self.F_bins = 50
         self.fac_exact = 1
+        
+        self.XX_independent = True
 
 
     def diag_likelihood(self, N, p00,p01,p10):
@@ -119,6 +121,8 @@ class Emily:
     def _ZZlk(self):
         hist = np.zeros(self.result_bins)
         histLB = np.zeros(self.result_bins)
+        self.ZZ_phist = [ np.zeros(self.prob_pts) for i in range(4) ]
+
         for i in range(self.blocks):
             pp00,pp01,pp10 = \
                     np.mgrid[i*self.prob_pts/self.blocks:(i+1)*self.prob_pts/self.blocks,\
@@ -128,11 +132,20 @@ class Emily:
                     range=(0,1), density=False, weights=plk)[0]
             histLB += np.histogram(pp01+pp10-2*np.sqrt(pp00*(1.-pp00-pp01-pp10)), 
                     bins=self.result_bins, range=(0,1), density=False, weights=plk)[0]
+            
+            for i,pp in enumerate([pp00,pp01,pp10,1.-pp00-pp01-pp10]):
+                self.ZZ_phist[i] += np.histogram(pp, bins=self.prob_pts,
+                        range=(0,1), density=False, weights=plk)[0]
 
         I = integrate.trapz(hist, dx=self.ZZ_dx)
         hist /= I
         ILB = integrate.trapz(histLB, dx=self.ZZ_dx)
         histLB /= ILB
+        
+        for h in self.ZZ_phist:
+            Ip = integrate.trapz(h, dx=1./self.prob_pts)
+            h /= Ip        
+        
         return hist, histLB
 
     def _XXlk(self, XmX=False):
@@ -141,26 +154,66 @@ class Emily:
         """
 
         hist_XX = np.zeros(self.result_bins)
+        self.XX_phist = [ np.zeros(self.prob_pts) for i in range(4) ]
+
         if XmX: 
             hist_XmX = np.zeros(self.result_bins)
-        for i in range(self.blocks):
-            aa,bb = np.mgrid[i*self.ab_pts/self.blocks:(i+1)*self.ab_pts/self.blocks,\
-                    0:self.ab_pts]/float(2*self.ab_pts)-0.25
-            plk_XX = self.offdiag_likelihood(self.N_XX, aa, bb)
-            hist_XX += np.histogram(4*aa, bins=self.result_bins,
-                range=(-1,1), density=False, weights=plk_XX)[0]
-            if XmX:
-                plk_XmX = self.offdiag_likelihood(self.N_XmX, aa, bb)
-                hist_XmX += np.histogram(-4*aa, bins=self.result_bins,
-                        range=(-1,1), density=False, weights=plk_XmX)[0]
+            self.XmX_phist = [ np.zeros(self.prob_pts) for i in range(4) ]
+        
+        if self.XX_independent:
+            for i in range(self.blocks):
+                pp00,pp01,pp10 = \
+                    np.mgrid[i*self.prob_pts/self.blocks:(i+1)\
+                    *self.prob_pts/self.blocks,\
+                    0:self.prob_pts,0:self.prob_pts]/float(self.prob_pts)
+                plk_XX = self.diag_likelihood(self.N_XX, pp00, pp01, pp10)
+                hist_XX += np.histogram(pp00+(1.-pp00-pp10-pp01)-pp01-pp10, 
+                        bins=self.result_bins,
+                        range=(-1,1), density=False, weights=plk_XX)[0]
+                
+                for i,pp in enumerate([pp00,pp01,pp10,1.-pp00-pp01-pp10]):
+                    self.XX_phist[i] += np.histogram(pp, bins=self.prob_pts,
+                            range=(0,1), density=False, weights=plk_XX)[0]
+                
+                if XmX:
+                    plk_XmX = self.diag_likelihood(self.N_XmX, pp00, pp01, pp10)
+                    hist_XmX += np.histogram(-(pp00+(1.-pp00-pp10-pp01)-pp01-pp10), 
+                            bins=self.result_bins,
+                            range=(-1,1), density=False, weights=plk_XmX)[0]
+                    
+                    for i,pp in enumerate([pp00,pp01,pp10,1.-pp00-pp01-pp10]):
+                        self.XmX_phist[i] += np.histogram(pp, bins=self.prob_pts,
+                                range=(0,1), density=False, weights=plk_XmX)[0]
+        
+        # TODO implement probability histograms for the restricted case as
+        # well
+        else:
+            for i in range(self.blocks):
+                aa,bb = np.mgrid[i*self.ab_pts/self.blocks:(i+1)*self.ab_pts/self.blocks,\
+                        0:self.ab_pts]/float(2*self.ab_pts)-0.25
+                plk_XX = self.offdiag_likelihood(self.N_XX, aa, bb)
+                hist_XX += np.histogram(4*aa, bins=self.result_bins,
+                    range=(-1,1), density=False, weights=plk_XX)[0]
+                if XmX:
+                    plk_XmX = self.offdiag_likelihood(self.N_XmX, aa, bb)
+                    hist_XmX += np.histogram(-4*aa, bins=self.result_bins,
+                            range=(-1,1), density=False, weights=plk_XmX)[0]
         
         I = integrate.trapz(hist_XX,dx=self.XX_dx)
         hist_XX /= I
+
+        for h in self.XX_phist:
+            Ip = integrate.trapz(h, dx=1./self.prob_pts)
+            h /= Ip  
         
         # we actually need the average of XX and XmX
         if XmX:
             I = integrate.trapz(hist_XmX, dx=self.XX_dx)
             hist_XmX /= I
+
+            for h in self.XX_phist:
+                Ip = integrate.trapz(h, dx=1./self.prob_pts)
+                h /= Ip  
 
             XXvals, XmXvals = np.meshgrid(self.XX_x, self.XX_x)
             hhXX, hhXmX = np.meshgrid(hist_XX, hist_XmX)
@@ -174,7 +227,7 @@ class Emily:
     def likelihood(self):
         t0 = time.time()
         
-        self.pvals = np.linspace(0.,1.,self.prob_pts)
+        self.pedges = np.linspace(0.,1.,self.prob_pts+1)
         self.ZZ_bin_edges = np.linspace(0,1,self.result_bins+1)
         self.ZZ_dx = self.ZZ_bin_edges[1] - self.ZZ_bin_edges[0]
         self.ZZ_x = self.ZZ_bin_edges[:-1] + self.ZZ_dx/2.
@@ -204,7 +257,7 @@ class Emily:
         print 'calculation took %.1f secs' % self.duration
     
     
-    def analysis(self, plot=True, do_fit=True, savefn=''):
+    def analysis(self, plot=True, do_fit=True, savefn='', save=True):
         # sign = -1 if self.state == 'psi2' else 1
 
         ### fit the Fidelity
@@ -233,7 +286,7 @@ class Emily:
 
         
         if plot:
-            fig = plt.figure(figsize=(8,8))
+            fig = plt.figure(figsize=(6,6))
             ax = fig.add_subplot(311)
             ax.plot(self.ZZ_x+self.ZZ_dx/2., self.ZZparity_lk, 'k', 
                     drawstyle='steps', label='excl sqrt')
@@ -257,7 +310,7 @@ class Emily:
             cx.plot(self.F_x+self.F_dx/2., self.FLB_lk, 'r', drawstyle='steps',
                     label='incl sqrt')
             if do_fit:
-                cx.plot(FLBfitx, FLBfity, 'r-', lw='2')            
+                cx.plot(FLBfitx, FLBfity, 'r-', lw='2')
             
             cx.set_xlabel('Fidelity')
             cx.set_ylabel('Likelihood')
@@ -271,13 +324,14 @@ class Emily:
 
             plt.tight_layout()
 
-            if not os.path.exists(self.savedir):
-                os.makedirs(self.savedir)
-            if savefn == '':
-                savefn = 'MLE'
+            if save:
+                if not os.path.exists(self.savedir):
+                    os.makedirs(self.savedir)
+                if savefn == '':
+                    savefn = 'MLE'
 
-            suffix = '_'+self.state
-            fig.savefig(os.path.join(self.savedir, savefn+suffix+'.png'))
+                suffix = '_'+self.state + ('_independentXX' if self.XX_independent else '')
+                fig.savefig(os.path.join(self.savedir, savefn+suffix+'.png'))
 
     def save(self, savefn=''):
         if not os.path.exists(self.savedir):
@@ -299,15 +353,35 @@ class Emily:
         for k in f.keys():
             setattr(self, k, f[k])
         f.close()
+
+
+class EmilyTest:
+
+    def __init__(self):
+        self.emily = Emily()
+        self.emily.F0_1 = 1.
+        self.emily.F1_1 = 1.
+        self.emily.F0_2 = 1.
+        self.emily.F1_2 = 1.
+        self.emily.fac_exact=0
+
+    def test_idealmixed(self):
+        self.emily.N_ZZ = np.array([10,10,10,10])
+        self.emily.N_XX = np.array([5,5,5,5])
+        self.emily.N_XmX = np.array([5,5,5,5])
+            
+        self.emily.likelihood()
+        self.emily.analysis()
+
+
         
 if __name__ == '__main__':
     emily = Emily()
     emily.likelihood()
     emily.analysis()
-    # emily.save()
+    emily.save()
+    
+    #et = EmilyTest()
+    #et.test_idealmixed()
 
 
-
-
-
-        
