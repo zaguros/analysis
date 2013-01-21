1cf6c9a70e967288fa89f1fc3aa55a7d23310574
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2013-01-21 15:14:14 +0100

    added weak measurement analysis scripts

--- lib/tools/plot.py
+++ lib/tools/plot.py
@@ -13,7 +13,6 @@ def plot_fit1d(res, fit_xvals, ax=None, ret=None, **kw):
     # know keywords:
     print_info = kw.pop('print_info', True)
     info_xy = kw.pop('info_xy', (0.15, 0.15))
-
     if res == None:
         return False
     
@@ -21,7 +20,10 @@ def plot_fit1d(res, fit_xvals, ax=None, ret=None, **kw):
         fig = plt.figure()
         ax = fig.add_subplot(111)
 
-    ax.plot(res['x'], res['y'], 'bo')
+    if 'yerr' in res.keys():
+        ax.errorbar(res['x'],res['y'],fmt='o',yerr=res['yerr'])
+    else:    
+        ax.plot(res['x'], res['y'], 'bo')
     ax.plot(fit_xvals, res['fitfunc'](fit_xvals), 'r-')
 
     if print_info:
