396514628a78d3f24000a358931ebff4788cc65e
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2013-02-22 13:01:35 +0100

    new stuff

--- scripts/lt1_local_teleportation/mbi_photon_ratio.py
+++ scripts/lt1_local_teleportation/mbi_photon_ratio.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import h5py
 
-fn = r'd:\measuring\data\20130212\191358_AdwinSSRO_SIL3_Ey_5nW\analysis.hdf5'
+fn = r'/Users/wp/Dropbox/Teleportation/Experiments/2013-01.. nuclei phases lt1-sil3/01 ssro/20130212/191358_AdwinSSRO_SIL3_Ey_5nW/analysis.hdf5'
 f = h5py.File(fn, 'r')
 
 pi_inv_prob = 0.95
@@ -25,6 +25,6 @@ ax = fig.add_subplot(111)
 
 ax.plot(t[1:], p_prj0[1:], 'o-')
 ax.set_xlabel('time (us)')
-ax.set_ylabel('probability for projection into 0')
+ax.set_ylabel('$P_{MBI}$')
 ax.set_xlim(0,50)
-ax.set_title('F(MBI)\n$F(\pi) = %.2f$' % pi_inv_prob)
+# ax.set_title('F(MBI)\n$F(\pi) = %.2f$' % pi_inv_prob)
