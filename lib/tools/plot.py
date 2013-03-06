027810fece0c40867b337d757b4d133aa4e78f4e
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2013-03-06 21:47:01 +0100

    worked a bit on handling of density matrices

--- lib/tools/plot.py
+++ lib/tools/plot.py
@@ -1,6 +1,8 @@
 import os, sys, time
+import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 from analysis.lib.fitting import fit
 
@@ -25,7 +27,7 @@ def plot_fit1d(res, fit_xvals, ax=None, ret=None, **kw):
     if plot_data:
         
         if 'yerr' in res.keys():
-            ax.errorbar(res['x'],res['y'],fmt='o',yerr=res['yerr'])
+            ax.errorbar(res['x'],res['y'], fmt='o', yerr=res['yerr'])
         else:    
             ax.plot(res['x'], res['y'], 'o')
     
@@ -51,5 +53,79 @@ def plot_fit1d(res, fit_xvals, ax=None, ret=None, **kw):
         return ax
 
     return
+
+#### Density matrix plotting
+def plot_dm(dm, **kw):
+    dmr = dm.real
+    dmi = dm.imag
+
+    fig = plt.figure(figsize=(12,4))
+    axr = fig.add_subplot(121, projection='3d')
+    axi = fig.add_subplot(122, projection='3d')
+
+    lx= len(dm[0])
+    ly= len(dm[:,0])
+    xticks = [(r'$| %d \rangle$' % i) for i in range(lx)]
+    yticks = [(r'$\langle %d |$' % i) for i in range(ly)]
+
+    plot_mat(dmr, axr, title=r'$\Re(\rho)$', 
+            xticks=xticks, yticks=yticks, **kw)
+    plot_mat(dmi, axi, title=r'$\Im(\rho)$', 
+            xticks=xticks, yticks=yticks, **kw)
+
+def plot_mat(mat, ax=None, **kw):
+    '''
+    plot a matrix as 3d bar chart.
+    '''
+    xlabel = kw.pop('xlabel', '')
+    ylabel = kw.pop('ylabel', '')
+    xticks = kw.pop('xticks', [])
+    yticks = kw.pop('yticks', [])
+    title = kw.pop('title', '')
+    zlabel = kw.pop('zlabel', '')
+    ret = kw.pop('ret', None)
+
+    if ax==None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+    
+    lx= len(mat[0])            # Work out matrix dimensions
+    ly= len(mat[:,0])
+    xpos = np.arange(0,lx,1)    # Set up a mesh of positions
+    ypos = np.arange(0,ly,1)
+    xpos, ypos = np.meshgrid(xpos+0.25, ypos+0.25)
+
+    xpos = xpos.flatten()   # Convert positions to 1D array
+    ypos = ypos.flatten()
+    zpos = np.zeros(lx*ly)
+
+    dx = 0.75 * np.ones_like(zpos)
+    dy = dx.copy()
+    dz = mat.flatten()
+
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz, alpha=0.5, color='RoyalBlue', 
+            zsort='average')
+
+    if xticks == []:
+        xticks = [str(i) for i in np.arange(lx)]
+    if yticks == []:
+        yticks = [str(i) for i in np.arange(ly)]
+    
+    ax.w_xaxis.set_ticks(np.arange(lx) + 0.5)
+    ax.w_yaxis.set_ticks(np.arange(ly) + 0.5)
+    ax.w_xaxis.set_ticklabels(xticks)
+    ax.w_yaxis.set_ticklabels(yticks)
+    ax.set_zlim3d([-1,1])
+    ax.set_zlabel(zlabel)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+
+    if ret == 'ax':
+        return ax
+
+    return
+
+
 
 
