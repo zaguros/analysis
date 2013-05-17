258a01f5ef505f384a432e33cdf4de738eba3b45
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2013-05-17 09:38:45 +0200

    updated analysis scripts

--- scripts/teleportation/dark_esr_analysis.py
+++ scripts/teleportation/dark_esr_analysis.py
@@ -6,7 +6,7 @@ import logging
 from matplotlib import pyplot as plt
 
 from analysis.lib import fitting
-from analysis.lib.m2.ssro import sequence_ssro
+from analysis.lib.m2.ssro import sequence
 from measurement.lib.tools import toolbox
 from analysis.lib.fitting import fit,esr
 from analysis.lib.tools import plot
@@ -29,9 +29,12 @@ else:
     folder = toolbox.latest_data('DarkESR')
 print folder
 
-a = sequence_ssro.DarkESRAnalysis(folder)
-x = a.get_sweep_pts()/1e6
-y = a.get_readout_results()
+a = sequence.SequenceAnalysis(folder)
+a.get_sweep_pts()
+a.get_readout_results('ssro')
+
+x = a.sweep_pts*1e3 # convert to MHz
+y = a.normalized_ssro
 
 # try fitting
 fit_result = fit.fit1d(x, y, esr.fit_ESR_gauss, guess_offset,
@@ -40,15 +43,15 @@ fit_result = fit.fit1d(x, y, esr.fit_ESR_gauss, guess_offset,
         # (2, guess_splitC),
         # (2, guess_splitB), 
         (3, guess_splitN), 
-        do_print=True, ret=True, fixed=[4,5])
+        do_print=True, ret=True, fixed=[4])
 ax = plot.plot_fit1d(fit_result, x, ret='ax', plot_data=True)
 
 ax.set_xlabel('MW frq (MHz)')
 ax.set_ylabel(r'uncorrected fidelity $F(|0\rangle)$')
 ax.set_title(a.timestamp+'\n'+a.measurementstring)
 
-plt.savefig(os.path.join(folder, 'darkesr_analysis.pdf'), 
-        format='pdf')
+plt.savefig(os.path.join(folder, 'darkesr_analysis.png'), 
+        format='png')
 
 
 
