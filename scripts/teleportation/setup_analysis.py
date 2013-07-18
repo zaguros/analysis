36b37ed343bf6d474ab2d23bba616d79326a16a2
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2013-07-18 09:33:36 +0200

    new analysis scripts

---
+++ scripts/teleportation/setup_analysis.py
@@ -0,0 +1,8 @@
+#few commands to get started quickly.
+
+sys.path.append(r'D://measuring/')
+
+from analysis.lib.m2.ssro import ssro,mbi
+from analysis.lib.tools import toolbox as tb
+
+import BSM_calibrations as bsmcal
