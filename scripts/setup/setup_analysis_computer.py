114f943470d0d79a1f6ff2f07ad912b49062804d
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2013-09-18 15:51:48 +0200

    added setup analysis computer

---
+++ scripts/setup/setup_analysis_computer.py
@@ -0,0 +1,13 @@
+#few commands to get started quickly.
+
+sys.path.append(r'D://measuring/')
+
+
+from analysis.lib.m2.ssro import ssro,mbi,sequence
+from analysis.lib.tools import toolbox as tb
+
+from analysis.script.teleportation import BSM_calibrations as bsmcal
+from analysis.script.teleportation import LT2_calibrations as lt2cal
+
+tb.datadir = r'x:\data'
+
