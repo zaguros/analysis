8c06f08da46c38394aa4188d7c09730c8a6a1a5a
Adriaan Rol <adriaan.rol@gmail.com>
2014-04-08 11:25:48 +0200

    DD_analysis_fingerprint script by THT

---
+++ scripts/QEC/DD_analysis_fingerprint.py
@@ -0,0 +1,57 @@
+'''Script to analyze the dynamical decoupling data
+by THT'''
+
+import numpy as np
+import os
+from measurement.lib.tools import toolbox
+from analysis.lib.tools import plot
+from analysis.lib.fitting import fit, common
+from analysis.lib.m2.ssro import mbi
+from matplotlib import pyplot as plt
+reload(common)
+
+## Data location ##
+measurement_name = ['adwindata']
+
+timestamp = ['20140407_223450', '20140407_223801', '20140407_224119', '20140407_224446', '20140407_231158','20140407_225215', '20140407_225614', '20140407_230023', '20140407_231645', '20140407_232118', '20140407_232603', '20140407_233057', '20140407_233605']#,
+
+#redo '20140407_224826' because it got corrupted
+
+cum_pts = 0
+cum_normalized_ssro = np.empty(0)
+
+for kk in range(len(timestamp)):
+    folder = toolbox.data_from_time(timestamp[kk])
+
+    a = mbi.MBIAnalysis(folder)
+    a.get_sweep_pts()
+    a.get_readout_results(name='adwindata')
+    a.get_electron_ROC()
+    cum_pts += a.pts
+
+    if kk == 0:
+        cum_sweep_pts = a.sweep_pts
+        cum_p0 = a.p0
+        cum_u_p0 = a.u_p0
+    else:
+        cum_sweep_pts = np.concatenate((cum_sweep_pts, a.sweep_pts))
+        cum_p0 = np.concatenate((cum_p0, a.p0))
+        cum_u_p0 = np.concatenate((cum_u_p0, a.u_p0))
+
+a.pts   = cum_pts
+a.sweep_pts = cum_sweep_pts
+a.p0    = cum_p0
+a.u_p0  = cum_u_p0
+
+ax = a.plot_results_vs_sweepparam(ret='ax')
+
+fig = a.default_fig(figsize=(6,4))
+ax = a.default_ax(fig)
+ax.plot(a.sweep_pts, a.p0, '.-b', lw=1)
+
+
+print folder
+plt.savefig(os.path.join(folder, 'combined_result.pdf'),
+format='pdf')
+plt.savefig(os.path.join(folder, 'combined_result.png'),
+format='png')
