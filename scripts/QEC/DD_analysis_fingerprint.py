64ed8e75dd818b656ebea169f06f75ee7cf24ccd
Computer <julia.cramer@gmail.com>
2014-04-14 13:43:05 +0200

    fingerprint

--- scripts/QEC/DD_analysis_fingerprint.py
+++ scripts/QEC/DD_analysis_fingerprint.py
@@ -15,7 +15,14 @@ reload(common)
 ## Data location ##
 measurement_name = ['adwindata']
 
-timestamp = ['20140407_223450', '20140407_223801', '20140407_224119', '20140407_224446', '20140407_231158','20140407_225215', '20140407_225614', '20140407_230023', '20140407_231645', '20140407_232118', '20140407_232603', '20140407_233057', '20140407_233605', '20140407_234654', '20140408_111324', '20140408_111725', '20140408_112126', '20140408_112529', '20140408_112930', '20140408_113614', '20140408_114015', '20140408_114416', '20140408_114818', '20140408_115220', '20140408_115622', '20140408_120024', '20140408_120426','20140408_120825', '20140408_130753','20140411_101732','20140411_102646','20140411_103753']#,
+timestamp = ['20140407_223450', '20140407_223801', '20140407_224119',
+'20140407_224446', '20140407_231158','20140407_225215', '20140407_225614',
+ '20140407_230023', '20140407_231645', '20140407_232118', '20140407_232603', 
+ '20140407_233057', '20140407_233605', '20140407_234654', '20140408_111324', 
+ '20140408_111725', '20140408_112126', '20140408_112529', '20140408_112930', 
+ '20140408_113614', '20140408_114015', '20140408_114416', '20140408_114818', 
+ '20140408_115220', '20140408_115622', '20140408_120024', '20140408_120426',
+ '20140408_120825', '20140408_130753']#,
 
 cum_pts = 0
 cum_normalized_ssro = np.empty(0)
@@ -49,9 +56,40 @@ fig = a.default_fig(figsize=(18,4))
 ax = a.default_ax(fig)
 ax.plot(a.sweep_pts, a.p0, '.-b', lw=1)
 
+
+timestamp = ['20140411_101732','20140411_102646','20140411_104011',
+'20140411_105345']
+cum_pts = 0
+cum_normalized_ssro = np.empty(0)
+
+for kk in range(len(timestamp)):
+    folder = toolbox.data_from_time(timestamp[kk])
+
+    a = mbi.MBIAnalysis(folder)
+    a.get_sweep_pts()
+    a.get_readout_results(name='adwindata')
+    a.get_electron_ROC()
+    cum_pts += a.pts
+
+    if kk == 0:
+        cum_sweep_pts = a.sweep_pts
+        cum_p0 = a.p0
+        cum_u_p0 = a.u_p0
+    else:
+        cum_sweep_pts = np.concatenate((cum_sweep_pts, a.sweep_pts))
+        cum_p0 = np.concatenate((cum_p0, a.p0))
+        cum_u_p0 = np.concatenate((cum_u_p0, a.u_p0))
+
+a.pts   = cum_pts
+a.sweep_pts = cum_sweep_pts
+a.p0    = cum_p0
+a.u_p0  = cum_u_p0
+
+
+ax.plot(a.sweep_pts, a.p0, '.-r', lw=1)
+
 cursor = Cursor(ax, useblit=True, color='gray', linewidth =1)
 
-print folder
 plt.savefig(os.path.join(folder, 'combined_result.pdf'),
 format='pdf')
 plt.savefig(os.path.join(folder, 'combined_result.png'),
@@ -60,7 +98,7 @@ format='png')
 zoom = 13.0
 fig = a.default_fig(figsize=(8,4))
 ax = a.default_ax(fig)
-ax.set_xlim(zoom-1000e-3,zoom+1000e-3)
+ax.set_xlim(12,14)
 ax.plot(a.sweep_pts, a.p0, '.-b', lw=1)
 
 
