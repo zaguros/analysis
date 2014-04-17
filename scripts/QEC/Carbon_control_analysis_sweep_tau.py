70580e706576472d589cd11204328bcd10cc1c9e
Team diamond lt2 <fdg@fdg.com>
2014-04-17 18:43:39 +0200

    analysis scripts MAR

--- scripts/QEC/Carbon_control_analysis_sweep_tau.py
+++ scripts/QEC/Carbon_control_analysis_sweep_tau.py
@@ -14,7 +14,7 @@ from analysis.lib.math import error
 import analysis.scripts.pulse_calibration.calibration_funcs as funcs
 
 ### parameters
-timestamp = '110229'#'175555' #'160434'
+timestamp = '154236'#'175555' #'160434'
 guess_x0 = 6.62
 guess_of = 0.1
 guess_a = 0
