1db013c616e9afbd4dd31cdfbae4ea32e4190429
Adriaan Rol <adriaan.rol@gmail.com>
2014-04-17 10:19:45 +0200

    again fixed numpy bug MAR

--- scripts/espin/electron_rabi_analysis.py
+++ scripts/espin/electron_rabi_analysis.py
@@ -62,7 +62,7 @@ y = a.p0
 fitfunc_str = 'o - A + A*e^(-kx)*cos(2pi (fx-phi))'
 
 def fitfunc(x):
-    return (o()-A()) + A() * exp(-k()*x) * cos(2*pi*(f()*x - phi()))
+    return (o()-A()) + A() * np.exp(-k()*x) * np.cos(2*np.pi*(f()*x - phi()))
 
 fit_result = fit.fit1d(x,y, None, p0=p0, fitfunc=fitfunc, fixed=[2],
         do_print=True, ret=True)
