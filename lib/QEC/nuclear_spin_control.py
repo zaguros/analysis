77559468b93faa4c03ee35c28931d642b059be27
Tim Taminiau <T.H.Taminiau@TUDelft.nl>
2014-04-29 08:41:20 +0200

    nuclear spin control theory

---
+++ lib/QEC/nuclear_spin_control.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+import analysis.lib.QEC.hyperfine_params as hf
+
+def C13_gate(carbon_number = 1, type = 'Ren', list_of_qubits = []):
+    pass
+
+def rotation_matrix(N = 1, tau = 10):
+    pass
+
+
