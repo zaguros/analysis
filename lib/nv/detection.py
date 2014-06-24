d97420e9327334225aeef9d5cd187ef175b9f03d
Bas Hensen <B.J.Hensen@tudelft.nl>
2014-06-24 11:51:05 +0200

    satureatio script update BASH

--- lib/nv/detection.py
+++ lib/nv/detection.py
@@ -6,7 +6,7 @@ def get_Ey_SSRO_stats_from_saturation(strain_splitting, saturation_counts):
 	real_countrates=saturation_counts*1/(1 - (dead_time*saturation_counts))
 
 	eta=real_countrates*NV_effective_lifetime
-	return get_RO_stats_from_detection_efficiency(strain_splitting, eta)
+	return get_Ey_SSRO_stats_from_detection_efficiency(strain_splitting, eta)
 
 def get_Ey_SSRO_stats_from_detection_efficiency(strain_splitting, eta):
 	
@@ -14,7 +14,7 @@ def get_Ey_SSRO_stats_from_detection_efficiency(strain_splitting, eta):
 	F0 = eta/(pflip+eta-pflip*eta) # = 1-Sum[(1-eta)^i *pflip*(1-pflip)^(i-1) ,{i,1,Infinity}]; 
 								   # see also page 135 of bas' master thesis
 	navg = eta/pflip
-	return F0, eta
+	return F0, navg
 
 def get_pflip(strain_splitting):
 	p0 = 0.012 #spin flip probability at zero strain
