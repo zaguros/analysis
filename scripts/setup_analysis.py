db99854e3da2284e008894f6e15d9552016a36ea
Team Diamond <cryolt2@gmail.com>
2014-07-08 15:02:24 +0200

    analysis bell Bash

--- scripts/setup_analysis.py
+++ scripts/setup_analysis.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 sys.path.append("d:/measuring")
 
 from analysis.lib.tools import toolbox as tb
@@ -8,6 +8,8 @@ from analysis.lib.m2.ssro import ssro, mbi, sequence, pqsequence
 from analysis.lib.nv import nvlevels
 from analysis.lib.lde import tail_cts_per_shot_v4 as tail
 from analysis.lib.pq import pq_tools, pq_plots
+from analysis.lib.math import readout_correction as roc
+from analysis.lib.math import error
 reload(m2)
 reload(tb)
 reload(ssro)
