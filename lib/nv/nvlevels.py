6948ec5d38a22f3ac5b86398d98f31b2603fb605
jcramer3 <j.cramer@tudelft.nl>
2014-08-29 16:25:43 +0200

    JC

--- lib/nv/nvlevels.py
+++ lib/nv/nvlevels.py
@@ -197,7 +197,7 @@ def get_optical_transitions(show_E_transitions=True,show_A_transitions=True,show
     A_transitions=np.array([E_ES[0]-E_GS[1],#E_ES[0]-E_GS[2],
                                  E_ES[1]-E_GS[2],#E_ES[1]-E_GS[1],
                                  E_ES[4]-E_GS[1],E_ES[4]-E_GS[2],
-                                 E_ES[5]-E_GS[1],E_ES[5]-E_GS[2]])  # 8 transitions
+                                 E_ES[5]-E_GS[1],E_ES[5]-E_GS[2]])  # 6 transitions
     E_prime_flip_transitions = np.array([E_ES[0]-E_GS[2],
                                         E_ES[1]-E_GS[1]])   # 4 transitions
     FB_E_transitions=np.array([E_ES[2]-E_GS[1],E_ES[2]-E_GS[2],
