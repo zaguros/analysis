6d8682f645367a658314059c4ca3ec00d6ebb117
machielblok <machielblok@gmail.com>
2015-01-09 14:44:14 +0100

    magnetometry: tools to compare different protocols -C

--- lib/magnetometry/plotting_tools.py
+++ lib/magnetometry/plotting_tools.py
@@ -2,6 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import rc, cm
+from analysis.lib.fitting import fit
 from analysis.lib.magnetometry import adaptive_magnetometry as magnetometry
 reload(magnetometry)
 load_data=False
@@ -13,7 +14,7 @@ def analyze_saved_simulations (timestamp,error_bars=False):
     mgnt_exp.calculate_scaling()
     return mgnt_exp
 
-def add_scaling_plot(timestamp, ax, exp_data, label, marker_settings, color):
+def add_scaling_plot(timestamp, ax, exp_data, label, marker_settings, color, return_all = False):
     #adds a scaling plot to axis 'ax', loading from analyzed data with a given 'timestamp'
     #exp_data=boolena, if 'True' then data is plotted with markers and errorbars are calculated, 
     #otherwise it is considered a simulation, and plotted like a line
@@ -23,7 +24,10 @@ def add_scaling_plot(timestamp, ax, exp_data, label, marker_settings, color):
     ax.plot (data_file.total_time, data_file.sensitivity, marker_settings,color=color, label=label)
     if exp_data: 
         ax.fill_between (data_file.total_time, data_file.sensitivity-data_file.err_sensitivity, data_file.sensitivity+data_file.err_sensitivity, color=color, alpha=0.2)
-    return ax
+    if return_all:
+        return ax, data_file.total_time, data_file.sensitivity, data_file.err_sensitivity, data_file.G, data_file.F, data_file.fid0
+    else:
+        return ax
 
 def compare_2plots(timestamp1, timestamp2, title):
     f = plt.figure(figsize=(8,6))
@@ -46,13 +50,13 @@ def compare_multiple_plots(timestamps, labels, title, colours = None):
     ax = f.add_subplot(1,1,1)
     markers = ['o', '^', 's','v', '>', '<',  '*', 'D', '+','|']
     ccc = np.linspace(0,1,len(timestamps))
+
     for i,k in enumerate(timestamps):
         if colours==None:
             c = cm.Set1(ccc[i])
         else:
             c = colours[i]
-        ax = add_scaling_plot (timestamp = k, exp_data=True, ax=ax, label = labels[i], marker_settings=markers[i], color=c)
-
+            ax = add_scaling_plot (timestamp = k, exp_data=True, ax=ax, label = labels[i], marker_settings=markers[i], color=c)
     ax.set_xscale('log')
     ax.set_yscale('log')
     ax.set_xlabel ('total ramsey time [$\mu$s]', fontsize=15)
@@ -61,4 +65,135 @@ def compare_multiple_plots(timestamps, labels, title, colours = None):
     for label in (ax.get_xticklabels() + ax.get_yticklabels()):
         label.set_fontsize(15)
     plt.legend()
+    plt.show()
+
+
+def generate_data_dict (timestamps, save_name = None):
+    data_dict = {}
+    for i,k in enumerate(timestamps):
+        data_file  = analyze_saved_simulations (timestamp=k,error_bars=True)
+        data_dict [k] = {'total_time': data_file.total_time, 'sensitivity':data_file.sensitivity, 'err_sensitivity':data_file.err_sensitivity, 'F':data_file.F, 'G':data_file.G, 'fid0':data_file.fid0}
+    
+    return data_dict
+
+
+def compare_best_sensitivities (data_dict_array, legend_array, title, colours=None):
+    #compare_multiple_plots must be run before, to have a data_dict
+
+    f = plt.figure(figsize=(8,6))
+    ax = f.add_subplot(1,1,1)
+    markers = ['o', '^', 's','v', '>', '<',  '*', 'D', '+','|']
+    ccc = np.linspace(0,1,len(data_dict_array))
+
+    idx = 0
+    plt.figure()    
+    for data_dict in data_dict_array:
+        list_F = []
+        list_sens = []
+        list_err_sens = []
+
+        for i,k in enumerate(data_dict):
+            list_F.append(data_dict[k]['F'])
+            sensitivity = data_dict[k]['sensitivity']
+            #print sensitivity
+            min_sens = min(sensitivity)
+            list_sens.append(min_sens)
+            min_idxs = [iii for iii, val in enumerate(sensitivity) if val == min_sens]  
+            m = min_idxs[0]
+            list_err_sens.append(data_dict[k]['err_sensitivity'][m])
+        
+        F = np.array(list_F)
+        ind = np.argsort(F)
+        F=F[ind]
+        sens = np.array(list_sens)[ind]
+        err_sens = np.array(list_err_sens)[ind]
+
+        if colours==None:
+            c = cm.Set1(ccc[idx])
+        else:
+            c = colours[idx]
+
+        ax.plot (F, sens, color=c, label = legend_array[idx])
+        ax.errorbar (F, sens,  yerr = err_sens, fmt=markers[idx], color=c)
+        idx = idx + 1
+
+    ax.set_yscale ('log')
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontsize(15)
+
+    #plt.fill_between (F, sens-err_sens, sens+err_sens, color='RoyalBlue', alpha=0.2)
+    ax.set_xlabel ('F', fontsize=15)
+    ax.set_ylabel ('best sensitivity [VH*T]', fontsize=15)
+    ax.set_title (title, fontsize=15)
+    ax.legend()
+    plt.show()
+
+
+
+def compare_scaling_fits (data_dict_array, legend_array, title='', colours=None, first_N = 3, last_N=10):
+    #compare_multiple_plots must be run before, to have a data_dict
+
+    f = plt.figure(figsize=(8,6))
+    ax = f.add_subplot(1,1,1)
+    markers = ['o', '^', 's','v', '>', '<',  '*', 'D', '+','|']
+    ccc = np.linspace(0,1,len(data_dict_array))
+
+    idx = 0
+    plt.figure()    
+    for data_dict in data_dict_array:
+        list_F = []
+        list_scaling = []
+        list_err_scaling = []
+
+        for i,k in enumerate(data_dict):
+            list_F.append(data_dict[k]['F'])
+            x0 = np.log10(data_dict[k]['total_time'][first_N:last_N]/20e-9)
+            y0 = np.log10(data_dict[k]['sensitivity'][first_N:last_N]/20e-9)
+            err_array = np.log10(data_dict[k]['err_sensitivity'][first_N:last_N]/20e-9)
+
+            guess_b = -1
+            guess_a = 0*y0[-1]+1
+            
+            a = fit.Parameter(guess_a, 'a')
+            b = fit.Parameter(guess_b, 'b')
+            
+            p0 = [a, b]
+            fitfunc_str = ''
+
+            def fitfunc(x):
+                return a()+b()*x
+
+
+            fit_result = fit.fit1d(x0,y0, None, p0=p0, fitfunc=fitfunc, fixed=[],
+                    do_print=False, ret=True, err_y = err_array)
+            b_fit = fit_result['params_dict']['b']
+            b_err = fit_result['error_dict']['b']
+            list_scaling.append(b_fit)
+            list_err_scaling.append(b_fit)
+
+        
+        F = np.array(list_F)
+        ind = np.argsort(F)
+        F=F[ind]
+        scaling_coeff = np.array(list_scaling)[ind]
+        err_scaling_coeff = np.array(list_err_scaling)[ind]
+
+        if colours==None:
+            c = cm.Set1(ccc[idx])
+        else:
+            c = colours[idx]
+
+        ax.plot (F, scaling_coeff, color=c, label = legend_array[idx])
+        ax.plot (F, scaling_coeff, markers[idx], color=c)
+        idx = idx + 1
+
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontsize(15)
+
+    #plt.fill_between (F, sens-err_sens, sens+err_sens, color='RoyalBlue', alpha=0.2)
+    ax.set_ylim([-1.1, 1.1])
+    ax.set_xlabel ('F', fontsize=15)
+    ax.set_ylabel ('scaling coefficient', fontsize=15)
+    ax.set_title (title, fontsize=15)
+    ax.legend()
     plt.show()
--- scripts/magnetometry/paper_figures/plot_suppl_material.py
+++ scripts/magnetometry/paper_figures/plot_suppl_material.py
@@ -12,16 +12,28 @@ reload(pt)
 load_data=False
 
 
-def compare_cappellaro_G3 ():
+def compare_cappellaro_G3_fid087 ():
     tag = 'simulated_adaptive_magnetometry'
-    t_stamps = []
-    labels = []
-    for i in np.arange(3):
-        t_stamps.append(tag+'_N=10G=3F='+str(i))
-        labels.append('G=3, F='+str(i))
+    t_stamps = ['20141215_152517', '20141215_152820', '20141215_153251', '20141215_153913', '20141215_154728', '20141215_155704']
+    labels = ['F=0', 'F=1', 'F=2', 'F=3', 'F=4', 'F=5']
+    colours = ['k', 'r', 'b', 'g', 'RoyalBlue', 'c']
+    pt.compare_multiple_plots (timestamps=t_stamps, labels=labels, title = 'adaptive phase update (G=3) - F0 = 0.87', colours=colours)
 
-    colours = ['b', 'r', 'k']
-    pt.compare_multiple_plots (timestamps=t_stamps, labels=labels, title = 'compare protocols', colours=colours)
+def compare_nonadaptive_G3_fid087 ():
+    t_stamps = ['20141215_152604', '20141215_152939', '20141215_153454', '20141215_154152', '20141215_155054', '20141215_160056']
+    labels = ['F=0', 'F=1', 'F=2', 'F=3', 'F=4', 'F=5']
+    colours = ['k', 'r', 'b', 'g', 'RoyalBlue', 'c']
+    pt.compare_multiple_plots (timestamps=t_stamps, labels=labels, title = 'non adaptive (G=3) - F0 = 0.87', colours=colours)
+
+t_stamps = ['20141215_152517', '20141215_152820', '20141215_153251', '20141215_153913', '20141215_154728', '20141215_155704']
+dict_G3_adptv = pt.generate_data_dict(timestamps = t_stamps)
+t_stamps = ['20141215_152604', '20141215_152939', '20141215_153454', '20141215_154152', '20141215_155054', '20141215_160056']
+dict_G3_nonAdptv = pt.generate_data_dict(timestamps = t_stamps)
+pt.compare_best_sensitivities ([dict_G3_adptv, dict_G3_nonAdptv], title = 'G=3 -- fid_0 = 0.87', legend_array = ['adptv', 'non adptv'])
+pt.compare_scaling_fits ([dict_G3_adptv, dict_G3_nonAdptv], title = 'G=3 -- fid_0 = 0.87', legend_array = ['adptv', 'non adptv'])
 
 
-compare_cappellaro_G3()
+
+#compare_cappellaro_G3_fid087()
+#compare_nonadaptive_G3_fid087()
+#20141215_152517_simulated_adaptive_magnetometry_N=10G=3F=0_fid0=0.87
