7d6249b6c21a3e8bce38eec479cef510838524cf
Team Diamond <cryolt2@gmail.com>
2016-01-13 10:03:33 +0100

    Purification fingerprint analysis
    SK and NK

--- lib/m2/ssro/sequence.py
+++ lib/m2/ssro/sequence.py
@@ -158,7 +158,7 @@ class SequenceAnalysis(m2.M2Analysis):
         
     def get_electron_ROC(self, **kw):
         ssro_calib_folder = kw.pop('ssro_calib_folder', toolbox.latest_data('SSROCalibration'))
-        print ssro_calib_folder
+        # print ssro_calib_folder
         if ssro_calib_folder == '':
                 ssro_calib_folder = toolbox.latest_data('SSROCalibration')
         self.p0 = np.zeros(self.normalized_ssro.shape)
