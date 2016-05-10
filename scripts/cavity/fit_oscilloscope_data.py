faba89a9302f0766872cb8be73fd009fa90993fb
coenen.lisanne <coenen.lisanne@gmail.com>
2016-05-10 11:26:32 +0200

    small changes, notebook scope analysis added

--- scripts/cavity/fit_oscilloscope_data.py
+++ scripts/cavity/fit_oscilloscope_data.py
@@ -9,7 +9,7 @@ from analysis.lib.tools import plot
 
 def get_linewidth(data,EOM_freq, g_a1 = 0.5, g_A1 = 0.04 , g_x01 = 0.0, g_gamma1 = 0.1, g_dx = 0.35, g_A2 = 0.02, g_gamma2 = 0.1, plot_data = True):
     """
-    load data, fit 3 lorentzians, get linewidth and the error
+    fit 3 lorentzians, get linewidth and the error
     input:
     data  = numpy ndarray with the data
     plot_data = False/True, default True
@@ -24,7 +24,7 @@ def get_linewidth(data,EOM_freq, g_a1 = 0.5, g_A1 = 0.04 , g_x01 = 0.0, g_gamma1
     lw = linewidth of middle peak
     u_lw = error of the linewidth
     """
-    x = data[:,0]*1.e5
+    x = data[:,0]*1.e5 # x-axis does not matter, factor for clarity. 
     y = data[:,1]
     n_xticks = 10
     fixed=[]
@@ -38,24 +38,45 @@ def get_linewidth(data,EOM_freq, g_a1 = 0.5, g_A1 = 0.04 , g_x01 = 0.0, g_gamma1
     gamma2 = fit_result['params_dict']['gamma2']
     u_gamma1 = fit_result['error_dict']['gamma1']
     scaling = EOM_freq/dx #scale the known EOM freq with the separation here.
-    lw = gamma1*scaling #scale the linewidth to get linewidht in frequency
+    lw = gamma1*scaling #scale the linewidth to get linewidth in frequency
     u_lw = u_gamma1*scaling
 
     fig,ax = plt.subplots(figsize=(8,4))
     plot.plot_fit1d(fit_result, np.linspace(x[0],x[-1],len(x)),ax=ax, label='Fit',show_guess=True, plot_data=True,color='red', data_linestyle = '-', print_info= False)
+    ax = set_axes(ax)
+
+    # ax.set_xlabel("Frequency (GHz)", fontsize = 14)
+    # ax.set_ylabel("Intensity (a.u.)", fontsize = 14)
+    # ax.set_xlim(x[0],x[-1])
+    # #rescaling x-axis in GHz
+    # X_min_freq = ax.get_xlim()[0]*scaling
+    # X_max_freq = ax.get_xlim()[-1]*scaling
+    # xticks = np.linspace(ax.get_xlim()[0],ax.get_xlim()[-1],n_xticks) 
+    # xticklabels = np.linspace(X_min_freq,X_max_freq,n_xticks)
+    # xticklabels_round=[]
+    # for j in xticklabels:
+    #   round_ = round(j,0)
+    #   xticklabels_round = np.append(xticklabels_round,round_)
+    # ax.set_xticklabels(xticklabels_round)
+    # ax.set_xticks(xticks)
+    plt.show()
+    return lw, u_lw, scaling
+
+
+def set_axes(ax):
     ax.set_xlabel("Frequency (GHz)", fontsize = 14)
     ax.set_ylabel("Intensity (a.u.)", fontsize = 14)
-    ax.set_xlim(x[0],x[-1])
-    #rescaling x-axis in GHz
-    X_min_freq = ax.get_xlim()[0]*scaling
-    X_max_freq = ax.get_xlim()[-1]*scaling
-    xticks = np.linspace(ax.get_xlim()[0],ax.get_xlim()[-1],n_xticks) 
-    xticklabels = np.linspace(X_min_freq,X_max_freq,n_xticks)
-    xticklabels_round=[]
-    for j in xticklabels:
-      round_ = round(j,0)
-      xticklabels_round = np.append(xticklabels_round,round_)
-    ax.set_xticklabels(xticklabels_round)
+
+    ax.grid(False)
+    ax.set_axis_bgcolor('white')
+    #ax.set_xlim(x[0],x[-1])
+
+    ax.tick_params(which = 'both', direction = 'out')
+    xticks = np.linspace(ax.get_xlim()[0],ax.get_xlim()[-1],int((ax.get_xlim()[0]-ax.get_xlim()[-1])/2+1))
+    xticklabels = np.linspace(ax.get_xlim()[0]*scaling,ax.get_xlim()[-1]*scaling,int((ax.get_xlim()[0]-ax.get_xlim()[-1])/2+1))
+    xticklabels = np.round(xticklabels,1)
+
     ax.set_xticks(xticks)
-    plt.show()
-    return lw, u_lw
+    ax.set_xticklabels(xticklabels, rotation=0)
+
+    return ax
