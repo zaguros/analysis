fdd7b0516730b7cbbbc06a78b5f98f6b89364294
Team Diamond <cryolt2@gmail.com>
2016-05-12 11:35:30 +0200

    Purification AR

--- lib/purification/purify_ssro.py
+++ lib/purification/purify_ssro.py
@@ -11,6 +11,7 @@ from analysis.lib.m2.ssro import mbi
 from matplotlib import pyplot as plt
 # import matplotlib as mpl
 from analysis.lib.fitting import fit, common
+import copy as cp
 
 reload(fit);reload(mbi);reload(common);reload(toolbox)
 
@@ -37,6 +38,8 @@ def create_plot(f,**kw):
 		plt.title(title + ' '+get_tstamp_from_folder(f))
 	else:
 		plt.title(get_tstamp_from_folder(f))
+
+	return fig,ax
     
     
 def save_and_close_plot(f):
@@ -64,7 +67,7 @@ def quadratic_addition(X,Y,X_u,Y_u):
 
 	return res, res_u
 
-def get_pos_neg_data(a,adwindata_str = '',**kw):
+def get_pos_neg_data(a,adwindata_str = '',ro_array = ['positive','negative'],**kw):
 	'''
 	Input: a : a data object of the class MBIAnalysis
 	averages positive and negative data
@@ -86,7 +89,7 @@ def get_pos_neg_data(a,adwindata_str = '',**kw):
 		return
 
 	##acquire pos_neg data
-	for i,ro in enumerate(['positive','negative']):
+	for i,ro in enumerate(ro_array):
 		a.get_sweep_pts()
 		a.get_readout_results(name = adwindata_str+ro,CR_after_check=CR_after_check)
 		a.get_electron_ROC(ssro_calib_folder)
@@ -132,15 +135,15 @@ def average_repump_time(contains = '',do_fit = False,**kw):
 		x,y1,y1_u  = get_pos_neg_data(a,adwindata_str = 'X_',**kw)
 		x2,y2,y2_u = get_pos_neg_data(a,adwindata_str = 'Y_',**kw)
 		y,y_u = quadratic_addition(y1,y2,y1_u,y2_u)
-		# y=y2
-		# y_u = y2_u
+		# y=y1
+		# y_u = y1_u
 		ylabel = 'Bloch vector length'
 
 
 	### create a plot
 	xlabel = a.g.attrs['sweep_name']
 	x = a.g.attrs['sweep_pts'] # could potentially be commented out?
-	create_plot(f,xlabel = xlabel,ylabel =ylabel,title = 'avg repump time')
+	fig,ax = create_plot(f,xlabel = xlabel,ylabel =ylabel,title = 'avg repump time')
 
 	### fitting if you feel like it / still needs implementation
 	if do_fit:
@@ -151,3 +154,165 @@ def average_repump_time(contains = '',do_fit = False,**kw):
 
 	## save and close plot. We are done.
 	save_and_close_plot(f)
+
+
+def number_of_repetitions(contains = '', do_fit = False, **kw):
+	'''
+	gets data from a folder whose name contains the contains variable.
+	Does or does not fit the data with a gaussian function
+	'''
+
+	### folder choice
+	if contains == '':
+		contains = '_sweep_number_of_reps'
+	elif len(contains) == 2:
+		contains = '_sweep_number_of_reps'+contains
+	elif len(contains) == 1:
+		contains = '_sweep_number_of_reps_'+contains
+
+
+	# older_than = kw.get('older_than',None) automatically handled by kws
+	### acquire data
+	f = toolbox.latest_data(contains,**kw)
+	a = mbi.MBIAnalysis(f)
+	
+	if '_Z' in f:
+		x,y,y_u = get_pos_neg_data(a,adwindata_str = 'Z_',**kw)
+		ylabel = 'Z'
+	else:
+		x,y1,y1_u  = get_pos_neg_data(a,adwindata_str = 'X_',**kw)
+		x2,y2,y2_u = get_pos_neg_data(a,adwindata_str = 'Y_',**kw)
+		y,y_u = quadratic_addition(y1,y2,y1_u,y2_u)
+		# y=y1
+		# y_u = y1_u
+		ylabel = 'Bloch vector length'
+
+
+	### create a plot
+	xlabel = a.g.attrs['sweep_name']
+	x = a.g.attrs['sweep_pts'] # could potentially be commented out?
+	fig,ax = create_plot(f,xlabel = xlabel,ylabel =ylabel,title = 'Number of repetitions')
+
+	### fitting if you feel like it / still needs implementation
+	if do_fit:
+		pass
+
+	## plot data
+	plot_data(x,y,y_u=y_u)
+
+	## save and close plot. We are done.
+	save_and_close_plot(f)
+
+
+def el_to_c_swap(contains = '',input_el=['Z'], do_fit = False, **kw):
+	'''
+	gets data from a folder whose name contains the contains variable.
+	Does or does not fit the data with a gaussian function
+	'''
+
+	### folder choice
+	if contains == '':
+		contains = 'Swap_el_to_C'
+
+	# older_than = kw.get('older_than',None) automatically handled by kws
+	### acquire data
+	f = toolbox.latest_data(contains,**kw)
+	a = mbi.MBIAnalysis(f)
+	print 'this is the timestamp ',get_tstamp_from_folder(f)
+
+	# data = np.empty([3,len(input_el)],dtype=str)
+	data = []
+	for i in range(len(input_el)):
+		data.append([0,0,0])
+	for ii,el in enumerate(input_el):
+		# data.append([0,0,0])
+		data_strings = []
+		ro_str = 'el_state_'+el+'_'
+
+		ro_array = ['positive','negative']
+		x,y,y_u = get_pos_neg_data(a,adwindata_str = ro_str,ro_array=ro_array,**kw)
+		y = np.round(y,decimals = 2)
+		y_u = np.round(y_u,decimals=2)
+		# print el,x,y,y_u ### for debugging
+
+		### put output string together
+		for jj,res,res_u in zip(range(3),y,y_u):
+			data[ii][jj] = cp.deepcopy(str(res) + " +/- "+ str(res_u))
+
+	row_format ="{:>18}" * (len(x) + 1)
+	headline_format = "{:>12}"+"{:>18}" * len(x)
+	print headline_format.format("", *x)
+	for el, row in zip(input_el, data):
+		print "--------------------------------------------------------------------------------------------------"
+		print row_format.format(el+' |', *row)
+
+
+
+
+
+def calibrate_LDE_phase(contains = '', do_fit = False, **kw):
+	'''
+	gets data from a folder whose name contains the contains variable.
+	Does or does not fit the data with a gaussian function
+	'''
+
+	### folder choice
+	if contains == '':
+		contains = 'LDE_phase_calibration'
+
+
+	# for fitting
+	freq = kw.pop('freq',1/12.) # voll auf die zwoelf.
+	fixed = kw.pop('fixed', [1])
+	show_guess = kw.pop('show_guess', False)
+
+	# older_than = kw.get('older_than',None) automatically handled by kws
+	### acquire data
+	f = toolbox.latest_data(contains,**kw)
+	a = mbi.MBIAnalysis(f)
+	
+	ro_array = ['positive','negative']
+	print ro_array
+	x,y,y_u = get_pos_neg_data(a,adwindata_str = 'X_',ro_array = ro_array,**kw)
+	ylabel = 'X'
+
+
+	### create a plot
+	xlabel = a.g.attrs['sweep_name']
+	x = a.g.attrs['sweep_pts'] # could potentially be commented out?
+	fig,ax = create_plot(f,xlabel = xlabel,ylabel =ylabel,title = 'Acquired phase')
+
+
+	## plot data
+	plot_data(x,y,y_u=y_u)
+
+		### fitting if you feel like it / still needs implementation
+	if do_fit:
+		A0 = max(y)
+		offset = 0
+		phi0 = 0
+		decay = 50e3
+
+		p0,fitfunc,fitfunc_str = common.fit_decaying_cos(freq,offset,A0,phi0,decay)
+
+		if show_guess:
+			# print decay
+			ax.plot(np.linspace(x[0],x[-1],201), fitfunc(np.linspace(x[0],x[-1],201)), ':', lw=2)
+
+		fit_result = fit.fit1d(x,y,None,p0 = p0, fitfunc = fitfunc, do_print = True, ret = True, fixed = fixed)
+		plot.plot_fit1d(fit_result, np.linspace(x[0],x[-1],1001), ax=ax, color = 'r', plot_data=False,add_txt = True, lw = 2)
+
+		p_dict = fit_result['params_dict']
+		e_dict = fit_result['error_dict']
+		detuning = a.g.attrs['phase_detuning']
+
+		if p_dict['A'] < 0:
+			p_dict['phi'] = p_dict['phi']+180
+			p_dict['A'] = p_dict['A']*(-1)
+
+		print 'This is the phase detuning', detuning
+		print 'acquired phase per repetition (includes phase detuning) {:3.3f} +/- {:3.3f}'.format(round(360*(p_dict['f']),3)-detuning,round(360*(e_dict['f']),3) )
+		print 'phase offset ', round(p_dict['phi'],3)
+		## save and close plot. We are done.
+	save_and_close_plot(f)
+
