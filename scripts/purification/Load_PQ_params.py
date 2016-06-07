92af5dc3725106127965ffd41665e0b04e243eaa
Team Diamond <cryolt2@gmail.com>
2016-06-07 15:26:45 +0200

    Purification AR

---
+++ scripts/purification/Load_PQ_params.py
@@ -0,0 +1,10 @@
+"""
+Loads analysis parameters for the purification project
+2016 NK
+"""
+
+def load_BK_params(a):
+	a.analysis_params = {}
+	a.w1_start = 1972e3
+	a.window_duration = 100e3
+	a.pulse_separation = 100e3
