7de290714e3c525ca37adb5ecca86042877fec1e
Teasm Diamond <cryolt2@gmail.com>
2016-07-20 14:14:42 +0200

    Purification analysis params

--- lib/purification/purify_analysis_params.py
+++ lib/purification/purify_analysis_params.py
@@ -4,11 +4,11 @@ Contains analysis parameters for purification measurements. Such as temporal fil
 
 filter_settings = {
 	
-	'st_start' 		: 2775e3,
+	'st_start' 		: 2774.5e3,
 	'st_len'		: 20e3,
 	'st_len_w2' 	: 20e3,
 	'max_reps_w1'	: 250,
 	'min_reps_w1'	: 1,
-	'max_reps_w2'	: 50,
+	'max_reps_w2'	: 30,
 	'min_reps_w2'	: 1,
 }
