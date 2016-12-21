2dda4cf1942719695468b3dfc927698dacb9c2b6
Team Diamond <cryolt2@gmail.com>
2016-12-21 16:01:54 +0100

    committing to LT3 Analysis

--- lib/purification/purify_pq.py
+++ lib/purification/purify_pq.py
@@ -14,6 +14,7 @@ class purifyPQAnalysis(pqsequence.PQSequenceAnalysis):
 	Combines pq measurements with adwin ssro.
 	"""
 	def __init__(self,folder,**kw):
+		print folder
 		pqsequence.PQSequenceAnalysis.__init__(self,folder,**kw)
 
 		self.agrp=self.adwingrp('adwindata')
