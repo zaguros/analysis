8da2b164a8554c06a00fb8460dfcdf276710f910
Tim Taminiau <T.H.Taminiau@TUDelft.nl>
2017-07-28 10:45:28 +0200

    by THT

--- lib/QEC/hyperfine_params.py
+++ lib/QEC/hyperfine_params.py
@@ -2,8 +2,28 @@
 ######### No1 SIL 18#####################################
 #########################################################
 
-# in Rad/Hz
+# in Hz
 
+### Note: these values are specific for the NV in the title above, please do not edit, make a library for your NV instead, THT
+
+hyperfine_params['C1']  = {'par' : -36.0e3, 'perp':25.0e3}
+hyperfine_params['C2']  = {'par' : 21.2e3, 'perp':43.0e3}
+hyperfine_params['C3']  = {'par' : -11.0e3, 'perp':55.0e3}
+hyperfine_params['C4']  = {'par' : 8.1e3, 'perp':21.0e3}
+hyperfine_params['C5']  = {'par' : 24.7e3, 'perp':26.0e3}
+hyperfine_params['C6']  = {'par' : -48.7e3, 'perp':12.0e3}
+hyperfine_params['C7']  = {'par' : 14.5e3, 'perp':11.0e3}
+hyperfine_params['C8']  = {'par' : 7.32e3, 'perp':5.0e3}
+
+
+# hyperfine_params['C8']  = {'par' : -20.5e3, 'perp':21.0e3}
+
+
+
+
+##########################################
+###### Values for some other unknown NV? #########
+##########################################
 
 ### fitted to the ms = -1 data
 
